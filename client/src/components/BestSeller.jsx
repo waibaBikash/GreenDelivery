bcc002a5ce3933f12a882fb99423fb08b60a9a44
waibaBikash <kavrelibikash@gmail.com>
2025-04-05 23:56:59 +1100

    create best seller

---
+++ client/src/components/BestSeller.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BestSeller = () => {
+  return (
+    <div className='mt-16'>
+       <p className='text-2xl md:text-3xl font-medium'>Best Seller</p>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default BestSeller
