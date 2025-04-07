def261246edc6ff9483f7b7b163fa173cd83d901
waibaBikash <kavrelibikash@gmail.com>
2025-04-07 21:09:33 +1000

    create newsletter

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
 import BestSeller from '../components/BestSeller'
 import ButtomBanner from '../components/ButtomBanner'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
        <Categories/>
        <BestSeller/>
        <ButtomBanner/>
+       <NewsLetter/>
     </div>
   )
 }
