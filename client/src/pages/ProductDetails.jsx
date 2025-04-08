9b51a729d6c799e4a09ddb9a55acce608e22b34a
waibaBikash <kavrelibikash@gmail.com>
2025-04-08 20:35:03 +1000

    show related products

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useAppContext } from '../context/AppContext'
 import { Link, useParams } from "react-router-dom";
 import { assets } from "../assets/assets";
+import ProductCard from "../components/ProductCard";
 const ProductDetails = () => {
 
   const {products, navigate, currency, addToCart} = useAppContext()
@@ -29,7 +30,7 @@ const ProductDetails = () => {
               <Link to={"/"}>Home</Link> /
               <Link to={"/products"}> Products</Link> /
               <Link to={`/porducts/${product.category.toLowerCase()}`}> {product.category}</Link> /
-              <span  className="text-indigo-500"> {product.name}</span>
+              <span  className="text-primary"> {product.name}</span>
           </p>
 
           <div className="flex flex-col md:flex-row gap-16 mt-4">
@@ -74,12 +75,25 @@ const ProductDetails = () => {
                       <button onClick={()=> addToCart(product._id)} className="w-full py-3.5 cursor-pointer font-medium bg-gray-100 text-gray-800/80 hover:bg-gray-200 transition" >
                           Add to Cart
                       </button>
-                      <button onClick={()=> {addToCart(product._id); navigate('/cart')}} className="w-full py-3.5 cursor-pointer font-medium bg-indigo-500 text-white hover:bg-indigo-600 transition" >
+                      <button onClick={()=> {addToCart(product._id); navigate('/cart')}} className="w-full py-3.5 cursor-pointer font-medium bg-primary text-white hover:bg-primary-dull transition" >
                           Buy now
                       </button>
                   </div>
               </div>
           </div>
+          {/* related products */}
+           <div className="flex flex-col items-center mt-20">
+             <div className="flex flex-col items-center w-max">
+                 <p className="text-3xl font-medium">Related Products</p>
+                 <div className="w-20 h-0.5 bg-primary rounded-full mt-2"></div>
+             </div>
+              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 lg:grid-cols-5 mt-6 w-full">
+                 {relatedProducts.filter((product)=> product.inStock).map((product, index)=> (
+                    <ProductCard key={index} product={product}/>
+                 ))}
+              </div>
+               <button onClick={()=> {navigate('/products'); scrollTo(0,0)}} className="mx-auto cursor-pointer px-12 my-16 py-2.5 border rounded text-primary hover:bg-primary/10 transition">See more</button>
+           </div>
       </div>
   );
 };
