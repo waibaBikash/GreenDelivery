83725a74292d190b13ee224fad734a48ad849d70
waibaBikash <kavrelibikash@gmail.com>
2025-04-08 20:46:47 +1000

    cart icon functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { useAppContext } from '../context/AppContext'
 const Navbar = () => {
   const [open, setOpen] = React.useState(false)
 
-  const { user, setUser, setShowUserLogin, navigate, searchQuery, setSearchQuery } = useAppContext();
+  const { user, setUser, setShowUserLogin, navigate, searchQuery, setSearchQuery, getCartCount } = useAppContext();
 
   const logout = async () => {
     setUser(null);
@@ -40,7 +40,7 @@ const Navbar = () => {
   
                   <div onClick={()=> navigate('/cart')} className="relative cursor-pointer">
                       <img src={assets.nav_cart_icon} alt="cart"  className='w-6 opacity-80'/>
-                      <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
+                      <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
                   </div>
                     {!user ? ( 
                   <button onClick={()=>setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -60,6 +60,29 @@ export const AppContextProvider = ({children}) => {
   toast.success("Remove form Cart")
   setCartItems(cartData)
  }
+
+ // Get Cart Item Count
+ const getCartCount = () => {
+  let totalCount = 0;
+  for(const item in cartItems){
+    totalCount += cartItems[item]
+  }
+  return totalCount;
+ }
+
+// Get Cart Total Amount
+ const getCartAmount = ()=> {
+   let totalAmount = 0;
+   for(const items in cartItems){
+    let itemInfo = products.find((product)=> product._id === items);
+    if(cartItems[items] > 0){
+      totalAmount += itemInfo.offerPrice * cartItems[items]
+    }
+   }
+   return Math.floor(totalAmount * 100) / 100;
+ }
+
+
   useEffect(()=>{
     fetchProducts()
   },[])
@@ -69,7 +92,7 @@ export const AppContextProvider = ({children}) => {
     setIsSeller, isSeller,showUserLogin, 
     setShowUserLogin, products, currency, 
     addToCart, updateCartItem, removeFromCart,
-    cartItems, searchQuery, setSearchQuery }
+    cartItems, searchQuery, setSearchQuery, getCartAmount, getCartCount }
   return <AppContext.Provider value={value}>
     {children}
   </AppContext.Provider>
