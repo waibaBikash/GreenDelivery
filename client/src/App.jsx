4be6b4a6bac75d945d49ae58a19a795d2d10296c
waibaBikash <kavrelibikash@gmail.com>
2025-04-08 20:21:22 +1000

    complete product details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
            <Route path='/' element = {<Home/>}/>
            <Route path='/products' element = {<AllProducts/>}/>
            <Route path='/products/:category' element = {<ProductCategory/>}/>
-           <Route path='/products/:category/:id' element = {<ProductDetails/>}/>
+           <Route path='/porducts/:category/:id' element = {<ProductDetails/>}/>
          </Routes>
        </div>
        {!isSellerPath && <Footer/>}
