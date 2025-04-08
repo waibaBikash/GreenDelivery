f2f5f7f157ea0ea3e05e0912d00cd5b54fbd267d
waibaBikash <kavrelibikash@gmail.com>
2025-04-08 20:49:19 +1000

    commit change

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -76,11 +76,11 @@ const Navbar = () => {
                      <button onClick={()=>{
                       setOpen(false);
                       setShowUserLogin(true);
-                     }} className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-primary-dull transition text-white rounded-full text-sm">
+                     }} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
                      Login
                  </button>
                   ) : (
-                    <button onClick={logout} className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-primary-dull transition text-white rounded-full text-sm">
+                    <button onClick={logout} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
                      Logout
                  </button>
                   )}
