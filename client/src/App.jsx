88d2a1234a19e750e5d868bc543cf0070720375b
waibaBikash <kavrelibikash@gmail.com>
2025-04-08 18:53:15 +1000

    create category

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Footer from './components/Footer'
 import { useAppContext } from './context/AppContext'
 import Login from './components/Login'
 import AllProducts from './pages/AllProducts'
+import ProductCategory from './pages/ProductCategory'
 
 const App = () => {
 
@@ -24,6 +25,7 @@ const App = () => {
          <Routes>
            <Route path='/' element = {<Home/>}/>
            <Route path='/products' element = {<AllProducts/>}/>
+           <Route path='/products/:category' element = {<ProductCategory/>}/>
          </Routes>
        </div>
        {!isSellerPath && <Footer/>}
