9bd791e179e181bc6c4fe0087c3acbd633d31c3a
waibaBikash <kavrelibikash@gmail.com>
2025-04-09 21:08:34 +1000

    showing cart item in mobile view

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -57,10 +57,16 @@ const Navbar = () => {
                     </div>
                   )}
               </div>
-  
-              <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
+
+              <div className='flex items-center gap-6 sm:hidden'>
+                   <div onClick={()=> navigate('/cart')} className="relative cursor-pointer">
+                      <img src={assets.nav_cart_icon} alt="cart"  className='w-6 opacity-80'/>
+                      <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
+                  </div>
+              <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu">
                   <img src={assets.menu_icon} alt="menu" />
               </button>
+              </div>
   
               {/* Mobile Menu */}
               { open && (
