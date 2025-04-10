d4605d1daad882583e68e3e4ff79c54223cc363f
waibaBikash <kavrelibikash@gmail.com>
2025-04-10 23:01:41 +1000

    create address page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import AllProducts from './pages/AllProducts'
 import ProductCategory from './pages/ProductCategory'
 import ProductDetails from './pages/ProductDetails'
 import Cart from './pages/Cart'
+import AddAddress from './pages/AddAddress'
 
 
 const App = () => {
@@ -31,6 +32,7 @@ const App = () => {
            <Route path='/products/:category' element = {<ProductCategory/>}/>
            <Route path='/porducts/:category/:id' element = {<ProductDetails/>}/>
            <Route  path='/cart' element={<Cart/>}/>
+           <Route  path='/add-address' element={<AddAddress/>}/>
          </Routes>
        </div>
        {!isSellerPath && <Footer/>}
