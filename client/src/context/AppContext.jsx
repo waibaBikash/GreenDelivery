108ed67b6f0ba95e20e24fe7c2153eb6b9ee4828
waibaBikash <kavrelibikash@gmail.com>
2025-04-11 21:25:10 +1000

    fix currency symbol shows on price

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContext = createContext();
 
 export const AppContextProvider = ({children}) => {
 
-  const currency = import.meta.VITE_CURRENCY;
+  const currency = import.meta.env.VITE_CURRENCY;
 
   const navigate = useNavigate();
   const [user, setUser] = useState(null)
