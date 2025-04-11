d652d959da3ffd10f93d080956c5c7fabb3a1af5
waibaBikash <kavrelibikash@gmail.com>
2025-04-11 21:21:19 +1000

    complete myorder page

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -26,6 +26,31 @@ const MyOrders = () => {
                <span>Payment: {order.paymentType}</span>
                <span>Total Amount: {currency}{order.amount}</span>
             </p>
+            {order.items.map((item, index)=>(
+              <div key={index}
+              className={`relative bg-white text-gray-500/70 ${
+              order.items.length !== index + 1 && "border-b"} border-gray-300 flex flex-col md:flex-row md:items-center justify-between p-4 py-5 md:gap-16 w-full max-w-4xl`}
+              >
+                 <div className='flex items-center mb-4 md:mb-0'>
+                    <div className='bg-primary/10 p-4 rounded-lg'>
+                       <img className='w-16 h-16' src={item.product.image[0]} alt="" />
+                    </div>
+                      <div className='ml-4'>
+                        <h2 className='text-xl font-medium text-gray-800'>{item.product.name}</h2>
+                        <p>category: {item.product.category}</p>
+                      </div>
+                  </div>
+
+                  <div className='text-primary text-lg font-medium'>
+                     <p>Quatity: {item.quantity || "1"}</p>
+                     <p>Status: {item.status}</p>
+                     <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
+                  </div>
+                   <p className='text-primary text-lg font-medium'>
+                      Amount: {currency}{item.product.offerPrice * item.quantity}
+                   </p>
+              </div>
+            ))}
           </div>
         ))}
     </div>
