1b38dd8907f89eecfdcb6d4c1291b90cfe613b34
waibaBikash <kavrelibikash@gmail.com>
2025-04-12 20:52:06 +1000

    create seller layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,14 +12,17 @@ import ProductDetails from './pages/ProductDetails'
 import Cart from './pages/Cart'
 import AddAddress from './pages/AddAddress'
 import MyOrders from './pages/MyOrders'
+import SellerLogin from './components/seller/SellerLogin'
+import SellerLayout from './pages/seller/SellerLayout'
 
 
 const App = () => {
 
+
   const isSellerPath = useLocation().pathname.includes("seller");
-  const {showUserLogin} = useAppContext()
+  const {showUserLogin, isSeller} = useAppContext()
   return (
-    <div>
+    <div className='text-default min-h-screen text-gray-700 bg-white'>
       {isSellerPath 
       ? null 
       : <Navbar/>}
@@ -35,6 +38,8 @@ const App = () => {
            <Route  path='/cart' element={<Cart/>}/>
            <Route  path='/add-address' element={<AddAddress/>}/>
            <Route  path='/my-orders' element={<MyOrders/>}/>
+           <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin/>} />
+
          </Routes>
        </div>
        {!isSellerPath && <Footer/>}
