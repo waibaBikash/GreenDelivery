94e7300c82bee90e5c35623247becc90588e06b9
waibaBikash <kavrelibikash@gmail.com>
2025-04-12 21:49:03 +1000

    save change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,7 +37,7 @@ const App = () => {
            <Route path='/' element = {<Home/>}/>
            <Route path='/products' element = {<AllProducts/>}/>
            <Route path='/products/:category' element = {<ProductCategory/>}/>
-           <Route path='/porducts/:category/:id' element = {<ProductDetails/>}/>
+           <Route path='/products/:category/:id' element = {<ProductDetails/>}/>
            <Route  path='/cart' element={<Cart/>}/>
            <Route  path='/add-address' element={<AddAddress/>}/>
            <Route  path='/my-orders' element={<MyOrders/>}/>
