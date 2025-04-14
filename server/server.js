9ceb6758f358949ae05675f0c1dd83342dbffd62
waibaBikash <kavrelibikash@gmail.com>
2025-04-14 14:59:30 +1000

    middleware congiguration

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 
+// Allow multiple origins
+const allowdOrigins = [
+  'http://localhost:5173']
 
+
+// Midldleware congiguration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin: allowdOrigins, Credential: true}));
 
 app.get('/', (req, res) => res.send('Api is Working!'));
 
