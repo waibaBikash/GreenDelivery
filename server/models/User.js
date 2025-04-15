37f4ade39329fb504af792c984af5b2b0c24f0ef
waibaBikash <kavrelibikash@gmail.com>
2025-04-15 19:59:34 +1000

    create user models

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+    cartItems: {
+    type: Object,
+    default: {}
+  },
+}, {minimize:false});
+
+const User = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default User;
