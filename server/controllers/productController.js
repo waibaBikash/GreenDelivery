76722075b52ee292dfc49f5c02d7caf863d4bb4e
waibaBikash <kavrelibikash@gmail.com>
2025-04-17 19:32:02 +1000

    add product functionality

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,7 +1,35 @@
-
+import { v2 as cloudinary } from "cloudinary";
+import Product from "../models/Product.js";
 // Add Product: /api/product/add
 export const addProduct = async (req, res) => {
+  try {
+    let productData = JSON.parse(req.body.productData);
+   
+    const images = req.files;
 
+    let imgagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url
+      })
+    );
+    await Product.create({
+      ...productData,
+      images: imgagesUrl,
+    });
+    res.json({
+      success: true,
+      message: "Product added successfully",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message
+    });
+  }
 }
 
 // Get Product: /api/product/list
