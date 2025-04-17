6eefd3f08b6f05d7d4bf3c5c60b7580309ec2e46
waibaBikash <kavrelibikash@gmail.com>
2025-04-17 19:37:07 +1000

    complete porduct controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -35,15 +35,53 @@ export const addProduct = async (req, res) => {
 // Get Product: /api/product/list
 
 export const productList = async (req, res) => {
-
+ try {
+   const products = await Product.find({});
+   res.json({
+     success: true,
+     products
+   })
+ } catch (error) {
+  console.log(error.message);
+   res.json({
+     success: false,
+     message: error.message
+   });
+ }
 }
 
 // Get single product: /api/product/id
 export const getProduct = async (req, res) => {
-
+  try {
+    const { id } = req.body;
+    const product = await Product.findById(id);
+    res.json({
+      success: true,
+      product
+    })
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message
+    });
+  }
 }
 
 // Change product inStock: /api/product/stock
 export const changeStock = async (req, res) => {
-
+  try {
+     const { id, inStock } = req.body;
+     await Product.findByIdAndUpdate(id, { inStock });
+     res.json({
+       success: true,
+       message: "Product stock updated successfully"
+     })
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message
+    });
+  }
 }
