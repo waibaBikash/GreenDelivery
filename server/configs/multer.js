fb8f06df931e7a6578ab9239b697cc3b7f1153e7
waibaBikash <kavrelibikash@gmail.com>
2025-04-17 19:19:19 +1000

    create multer function

---
+++ server/configs/multer.js
@@ -0,0 +1,24 @@
+import multer from "multer";
+
+
+
+export const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + "-" + file.originalname);
+    },
+  }),
+  limits: { fileSize: 1000000 }, // 1 MB
+  fileFilter: (req, file, cb) => {
+    const filetypes = /jpeg|jpg|png|gif/;
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+    cb("Error: File upload only supports the following filetypes - " + filetypes);
+  },
+});
