e16c8febba21ed655fbc3680697337ece83680d3
waibaBikash <kavrelibikash@gmail.com>
2025-04-17 18:51:48 +1000

    create product model

---
+++ server/controllers/productController.js
@@ -0,0 +1,21 @@
+
+// Add Product: /api/product/add
+export const addProduct = async (req, res) => {
+
+}
+
+// Get Product: /api/product/list
+
+export const productList = async (req, res) => {
+
+}
+
+// Get single product: /api/product/id
+export const getProduct = async (req, res) => {
+
+}
+
+// Change product inStock: /api/product/stock
+export const changeStock = async (req, res) => {
+
+}
