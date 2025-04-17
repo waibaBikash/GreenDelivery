2b95e592dbc71ca9f8cf64b684cde7a23695f8ac
waibaBikash <kavrelibikash@gmail.com>
2025-04-17 14:29:43 +1000

    create seller login

---
+++ server/controllers/sellerController.js
@@ -0,0 +1,25 @@
+
+import jwt from 'jsonwebtoken';
+// Login seller : /api/seller/login
+
+export const sellerLogin = async (req, res) => {
+  const { email, password } = req.body;
+  
+  try {
+    if(password === process.env.SELLER_PASSWORD && email === process.env.SELLER_EMAIL) {
+      const token = jwt.sign({email}, process.env.JWT_SECRET, {expiresIn: '7d'});
+      res.cookie('sellerToken', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'porduction' ? 'none' : 'strict',
+        maxage: 7 * 24 * 60 * 60 * 1000,
+      });
+      return res.json({success: true, message: 'Login successful'});
+    } else{
+      return res.json({success: false, message: 'Invalid credentials'});
+    }
+  } catch (error) {
+    console.log(error.message);
+    return res.json({success: false, message: error.message});
+  }
+}
