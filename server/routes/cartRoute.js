6857d92410a9435281d2d2187bc0920e20276e57
waibaBikash <kavrelibikash@gmail.com>
2025-04-19 19:42:34 +1000

    update api cart data

---
+++ server/routes/cartRoute.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import authUser from "../middlewares/authUser.js";
+import { updateCart } from "../controllers/cartController.js";
+
+
+const cartRouter = mongoose.Router();
+
+cartRouter.post('/update', authUser, updateCart);
+
+
+export default cartRouter;
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
 import connectCloudinary from './configs/cloudinary.js';
 import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
 
 
@@ -31,6 +32,7 @@ app.get('/', (req, res) => res.send('API is Working!'));
 app.use('/api/user', userRouter)
 app.use('/api/seller', sellerRouter)
 app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost: ${port}`);
