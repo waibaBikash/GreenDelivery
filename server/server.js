015c56499af321d409f3a4f19a3a4c5a42385ec0
waibaBikash <kavrelibikash@gmail.com>
2025-04-19 20:07:23 +1000

    api for add and get user

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import sellerRouter from './routes/sellerRoute.js';
 import connectCloudinary from './configs/cloudinary.js';
 import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
+import addressRouter from './routes/addressRoute.js';
 
 
 
@@ -33,6 +34,7 @@ app.use('/api/user', userRouter)
 app.use('/api/seller', sellerRouter)
 app.use('/api/product', productRouter)
 app.use('/api/cart', cartRouter)
+app.use('/api/address', addressRouter)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost: ${port}`);
