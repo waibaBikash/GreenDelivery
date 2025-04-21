232cf78d4f3a6bf898e801cb9946451fd22d68ef
waibaBikash <kavrelibikash@gmail.com>
2025-04-21 20:24:52 +1000

    refactor logout functionality to use axios for API call and handle responses with toast notifications

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -1,10 +1,11 @@
 import { Link, NavLink, Outlet } from "react-router-dom";
 import { assets } from "../../assets/assets";
 import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const SellerLayout = () => {
 
-  const { setIsSeller } = useAppContext()
+  const { axios, navigate } = useAppContext()
 
 
   const sidebarLinks = [
@@ -13,8 +14,19 @@ const SellerLayout = () => {
       { name: "Orders", path: "/seller/orders", icon: assets.order_icon },
   ];
 
-  const logout = ()=>{
-    setIsSeller(false);
+  const logout = async ()=>{
+    try {
+        const {data} = await axios.get("/api/seller/logout");
+        if(data.success){
+            toast.success(data.message)
+            navigate("/")
+        }else{
+            toast.error(data.message)
+        }
+        
+    } catch (error) {
+        toast.error(error.message)
+    }
   }
 
   return (
