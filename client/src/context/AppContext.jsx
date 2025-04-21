1946ddc896f0080dd866931e58bcc938e94a25e5
waibaBikash <kavrelibikash@gmail.com>
2025-04-21 19:58:46 +1000

    set up axios defaults for backend URL and credentials

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,6 +4,8 @@ import { dummyProducts } from '../assets/assets';
 import toast from 'react-hot-toast';
 import axios from 'axios';
 
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
 export const AppContext = createContext();
 
