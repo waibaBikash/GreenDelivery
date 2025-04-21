2cdf6a83c2aab3e39d56ac16f05b8c43c4f7fc8c
waibaBikash <kavrelibikash@gmail.com>
2025-04-21 21:04:53 +1000

    refactor NavLink rendering for improved readability

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -45,7 +45,10 @@ const SellerLayout = () => {
           <div className="flex">
           <div className="md:w-64 w-16 border-r h-[95vh] text-base border-gray-300 pt-4 flex flex-col">
               {sidebarLinks.map((item) => (
-                  <NavLink to={item.path} key={item.name} end={item.path === "/seller"}
+                  <NavLink
+                   to={item.path}
+                   key={item.name} 
+                   end={item.path === "/seller"}
                       className={({isActive})=>`flex items-center py-3 px-4 gap-3 
                           ${isActive ? "border-r-4 md:border-r-[6px] bg-primary/10 border-primary text-primary"
                               : "hover:bg-gray-100/90 border-white "
