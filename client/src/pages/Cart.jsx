7d465238baa2888a73b02aef3139355d55804821
waibaBikash <kavrelibikash@gmail.com>
2025-04-22 19:43:36 +1000

    feat: implement user address fetching with error handling and default selection

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 import { useAppContext } from "../context/AppContext";
 import { assets, dummyAddress } from "../assets/assets";
+import toast from "react-hot-toast";
 
 const Cart = () => {
 
-  const { products, currency, cartItems, removeFromCart, getCartCount, updateCartItem, navigate, getCartAmount} = useAppContext()
+  const { products, currency, cartItems, removeFromCart, getCartCount, updateCartItem, navigate, getCartAmount, axios,user} = useAppContext()
   const [cartArray, setCartArray] = useState([])
-  const [addresses, setAddresses] = useState([dummyAddress])
+  const [addresses, setAddresses] = useState([])
   const [showAddress, setShowAddress] = useState(false)
-  const [selectedAddresses, setSelectedAddresses] = useState(dummyAddress[0])
+  const [selectedAddresses, setSelectedAddresses] = useState(null)
   const [paymentOption, setPaymentOption] = useState("COD")
 
   const getCart = () => {
@@ -21,6 +22,22 @@ const Cart = () => {
     setCartArray(temArray)
   }
 
+  const getUserAddress = async () => {
+    try {
+        const {data} = await axios.get("/api/address/get");
+        if(data.success){
+          setAddresses(data.addresses);
+           if(data.addresses.length > 0){
+            setSelectedAddresses(data.addresses[0]);
+           }
+        }else{
+            toast.error(data.message);
+        }
+    } catch (error) {
+        toast.error(error.message);
+    }
+  }
+
   const placeOrder = async () => {
 
   }
@@ -30,6 +47,12 @@ const Cart = () => {
      getCart()
    }
   },[products, cartItems])
+
+    useEffect(()=>{
+       if(user){
+         getUserAddress()
+       }
+    },[user])
 
   return products.length > 0 && cartItems ? (
       <div className="flex flex-col md:flex-row mt-6">
