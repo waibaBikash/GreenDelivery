2cd8331b7418dc29dc5e815c0cf0744e89681959
waibaBikash <kavrelibikash@gmail.com>
2025-04-22 19:36:57 +1000

    feat: add address submission functionality with success and error notifications

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
+import React, { use, useState } from 'react'
 import { assets } from '../assets/assets'
+import { useAppContext } from '../context/AppContext';
+import toast from 'react-hot-toast';
 
 
 // Input Field Comments
@@ -14,6 +16,8 @@ const InputField = ({type, placeholder, name, handleChange, address} )=>(
   />
 )
 const AddAddress = () => {
+
+  const { axios, user, navigate} = useAppContext();
 
   const [address, setAddress] = useState({
     firstName: '',
@@ -37,7 +41,25 @@ const AddAddress = () => {
 
   const onSubmitHandler = async (e) => {
    e.preventDefault();
+   try {
+    const {data} = await axios.post("/api/address/add", {address})
+    if(data.success){
+      toast.success(data.message);
+      navigate("/cart");
+    }
+    else{
+      toast.error(data.message);
+    }
+   } catch (error) {
+    toast.error(error.message);
+   }
   }
+
+  useEffect(()=>{
+    if(!user){
+      navigate("/cart");
+    }
+  },[])
   return (
     <div className='mt-16 pb-16'>
       <p className='text-2xl md:text-3xl text-gray-500'>Add Shipping 
