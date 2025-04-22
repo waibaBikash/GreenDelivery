ed6163131e62ec5df112cb3f1c359dc7a7c016ef
waibaBikash <kavrelibikash@gmail.com>
2025-04-22 19:10:18 +1000

    add axios to Navbar for logout functionality with error handling and success feedback

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,11 +6,21 @@ import { useAppContext } from '../context/AppContext'
 const Navbar = () => {
   const [open, setOpen] = React.useState(false)
 
-  const { user, setUser, setShowUserLogin, navigate, searchQuery, setSearchQuery, getCartCount } = useAppContext();
+  const { user, setUser, setShowUserLogin, navigate, searchQuery, setSearchQuery, getCartCount, axios } = useAppContext();
 
   const logout = async () => {
-    setUser(null);
-    navigate('/')
+    try {
+      const { data } = await axios.get('/api/user/logout');
+      if (data.success) {
+        toast.success(data.message);
+        setUser(null);
+        navigate('/');
+      }else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   }
 
   useEffect(()=>{
