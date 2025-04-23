2ffcc0436b338d05cc59ab4b2feddac210f29c2e
waibaBikash <kavrelibikash@gmail.com>
2025-04-23 20:49:16 +1000

    feat: implement API call to fetch seller orders with error handling

--- client/src/pages/seller/Orders.jsx
+++ client/src/pages/seller/Orders.jsx
@@ -1,14 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import { useAppContext } from '../../context/AppContext'
 import { assets, dummyOrders } from '../../assets/assets'
+import toast from 'react-hot-toast'
 
 const Orders = () => {
 
-  const {currency} = useAppContext()
+  const {currency, axios} = useAppContext()
   const [orders, setOrders] = useState([])
 
   const fetchOrders = async () => {
-     setOrders(dummyOrders)
+     try {
+        const {data} = await axios.get("/api/order/seller");
+        if(data.success){
+          setOrders(data.orders);
+        }else{
+            toast.error(data.message);
+        }
+     } catch (error) {
+        toast.error(error.message);
+     }
   }
 
 
