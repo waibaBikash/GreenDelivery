43917fbdac93b3c0217a6319b4a8e4b544424a04
waibaBikash <kavrelibikash@gmail.com>
2025-04-23 20:46:44 +1000

    fix: format code for consistency in addressController.js

--- server/controllers/addressController.js
+++ server/controllers/addressController.js
@@ -1,12 +1,14 @@
 
 
+import Address from '../models/Address.js';
+
 // Add Address: /api/address/add
 
 
 
 export const addAddress = async (req, res) => {
   try {
-    const { address, userId}= req.body;
+    const { address, userId} = req.body;
     await Address.create({...address, userId});
     // await Address.create({...address, userId});
     res.json({
