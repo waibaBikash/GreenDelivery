448234e35cff96b8750c949dac70a68d8eaacf50
waibaBikash <kavrelibikash@gmail.com>
2025-04-27 19:53:27 +1000

    fix: improve formatting and readability in Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,10 +4,13 @@ const Footer = () => {
   
   return (
       <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-24 bg-primary/10">
-          <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
+          <div 
+           className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
               <div>
                   <img className="w-34 md:w-32" src={assets.logo} alt="logo" />
-                  <p className="max-w-[410px] mt-6">We deliver fresh groceries and snacks straight to your door. Trusted by thousands, we aim to make your shopping experience simple and assortable.</p>
+                  <p className="max-w-[410px] mt-6">We deliver fresh groceries and snacks straight 
+                  to your door. Trusted by thousands, we aim to make your shopping experience simple and assortable.
+                  </p>
               </div>
               <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
                   {footerLinks.map((section, index) => (
