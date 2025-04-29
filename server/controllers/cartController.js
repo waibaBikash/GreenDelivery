a98eb5bfc3bda1241213cf1e66e54e660d7414c2
waibaBikash <kavrelibikash@gmail.com>
2025-04-29 21:12:23 +1000

    fix: streamline updateCart function by improving formatting in findByIdAndUpdate call

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -7,11 +7,10 @@ import User from "../models/User.js";
 export const updateCart = async (req, res) => {
   try {
     const { userId, cartItems } = req.body;
-    await User.findByIdAndUpdate(
-      userId,{
+    await User.findByIdAndUpdate(userId,{
         cartItems
       }
-    );
+    )
     res.json({
       success: true,
       message: "Cart updated successfully",
