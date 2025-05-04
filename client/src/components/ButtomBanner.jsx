f270152e1aefcc93315cb400a094c8dccb2022d5
waibaBikash <kavrelibikash@gmail.com>
2025-05-04 21:19:37 +1000

    fix: format JSX for consistency in ButtomBanner component

--- client/src/components/ButtomBanner.jsx
+++ client/src/components/ButtomBanner.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import { assets, features } from '../assets/assets'
+import React from 'react';
+import { assets, features } from '../assets/assets';
 
 const ButtomBanner = () => {
   return (
-    <div className='relative mt-24'>
+    <div 
+       className='relative mt-24'>
         <img className='w-full hidden md:block' src={assets.bottom_banner_image} alt="banner" />
         <img className='w-full md:hidden' src={assets.bottom_banner_image_sm} alt="banner" />
 
-        <div className='absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24'>
+        <div 
+         className='absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24'>
             <div>
-               <h1 className='text-2xl md:text-3xl font-semibold text-primary mb-6'>Why We Are The Best</h1>
+               <h1 
+                className='text-2xl md:text-3xl font-semibold text-primary mb-6'>
+                 Why We Are The Best
+                </h1>
                {features.map((feature, index)=>(
                  <div key={index} className='flex items-center gap-4 mt-2'>
                     <img className='md:w-11 w-9' src={feature.icon} alt={feature.title} />
@@ -25,4 +30,4 @@ const ButtomBanner = () => {
   )
 }
 
-export default ButtomBanner
+export default ButtomBanner;
