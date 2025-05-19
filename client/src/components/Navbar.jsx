8e8118a60eb38b5787b406c7b020591a8d6a049d
waibaBikash <kavrelibikash@gmail.com>
2025-05-19 20:17:12 +1000

    fix: standardize formatting in Navbar component for improved readability

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -30,7 +30,8 @@ const Navbar = () => {
   },[searchQuery])
   
   return (
-          <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
+          <nav 
+          className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
   
               <NavLink to='/' onClick={()=> setOpen(false)}>
                   <img src={assets.logo} alt="logo" />
@@ -43,35 +44,53 @@ const Navbar = () => {
                   <NavLink to='/'>Contact</NavLink>
                   
   
-                  <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-                      <input onChange={(e)=> setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
-                       <img src={assets.search_icon} alt="search" className='w-4 h-4' />
+                  <div 
+                   className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
+                      <input
+                       onChange={(e)=> setSearchQuery(e.target.value)}
+                       className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" 
+                       type="text" 
+                       placeholder="Search products" />
+                       <img src={assets.search_icon} alt="search" 
+                       className='w-4 h-4' />
                   </div>
   
-                  <div onClick={()=> navigate('/cart')} className="relative cursor-pointer">
-                      <img src={assets.nav_cart_icon} alt="cart"  className='w-6 opacity-80'/>
-                      <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
+                  <div onClick={()=> navigate('/cart')}
+                   className="relative cursor-pointer">
+                      <img src={assets.nav_cart_icon} alt="cart"  
+                      className='w-6 opacity-80'/>
+                      <button
+                       className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}
+                       </button>
                   </div>
                     {!user ? ( 
-                  <button onClick={()=>setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+                  <button onClick={()=>setShowUserLogin(true)} 
+                  className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
                       Login
                   </button> )
                   :
                   (
                     <div className='relative group'>
-                      <img src={assets.profile_icon} alt=""  className='w-10'/>
+                      <img src={assets.profile_icon} alt="" 
+                       className='w-10'/>
                       <ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40'>
-                         <li onClick={()=> navigate('/my-orders')} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
-                         <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
+                         <li onClick={()=> navigate('/my-orders')} 
+                         className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
+                         <li onClick={logout}
+                          className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
                       </ul>
                     </div>
                   )}
               </div>
 
               <div className='flex items-center gap-6 sm:hidden'>
-                   <div onClick={()=> navigate('/cart')} className="relative cursor-pointer">
-                      <img src={assets.nav_cart_icon} alt="cart"  className='w-6 opacity-80'/>
-                      <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
+                   <div onClick={()=> navigate('/cart')} 
+                   className="relative cursor-pointer">
+                      <img src={assets.nav_cart_icon} alt="cart"  
+                      className='w-6 opacity-80'/>
+                      <button 
+                      className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}
+                      </button>
                   </div>
               <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu">
                   <img src={assets.menu_icon} alt="menu" />
@@ -86,7 +105,8 @@ const Navbar = () => {
                   { user &&
                   <NavLink to='/products' onClick={()=>setOpen(false)}>My Orders</NavLink>
                   }
-                  <NavLink to='/' onClick={()=>setOpen(false)}>Contact</NavLink>
+                  <NavLink to='/'
+                   onClick={()=>setOpen(false)}>Contact</NavLink>
 
                   {!user ? (
                      <button onClick={()=>{
@@ -96,7 +116,8 @@ const Navbar = () => {
                      Login
                  </button>
                   ) : (
-                    <button onClick={logout} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
+                    <button onClick={logout} 
+                    className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
                      Logout
                  </button>
                   )}
