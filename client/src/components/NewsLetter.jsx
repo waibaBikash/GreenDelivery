b06806cf3a8ec04e34bfbb083f80ca943011627a
waibaBikash <kavrelibikash@gmail.com>
2025-05-19 20:18:03 +1000

    fix: standardize formatting in ProductCard component for improved readability

--- client/src/components/NewsLetter.jsx
+++ client/src/components/NewsLetter.jsx
@@ -1,7 +1,8 @@
 const NewsLetter = () => {
     
   return (
-      <div className="flex flex-col items-center justify-center text-center space-y-2 mt-24 pb-14">
+      <div 
+      className="flex flex-col items-center justify-center text-center space-y-2 mt-24 pb-14">
           <h1 className="md:text-4xl text-2xl font-semibold">Never Miss a Deal!</h1>
           <p className="md:text-lg text-gray-500/70 pb-8">
               Subscribe to get the latest offers, new arrivals, and exclusive discounts
@@ -13,7 +14,8 @@ const NewsLetter = () => {
                   placeholder="Enter your email id"
                   required
               />
-              <button type="submit" className="md:px-12 px-8 h-full text-white bg-primary hover:bg-primary-dull transition-all cursor-pointer rounded-md rounded-l-none">
+              <button type="submit" 
+              className="md:px-12 px-8 h-full text-white bg-primary hover:bg-primary-dull transition-all cursor-pointer rounded-md rounded-l-none">
                   Subscribe
               </button>
           </form>
--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -6,7 +6,8 @@ const ProductCard = ({product}) => {
   const {currency, addToCart, removeFromCart, cartItems, navigate} = useAppContext()
 
   return product && (
-      <div onClick={()=>{navigate(`/products/${product.category.toLowerCase()}/${product._id}`); scrollTo(0,0)}}
+      <div 
+      onClick={()=>{navigate(`/products/${product.category.toLowerCase()}/${product._id}`); scrollTo(0,0)}}
         className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
           <div className="group cursor-pointer flex items-center justify-center px-2">
               <img className="group-hover:scale-105 transition max-w-26 md:max-w-36"
@@ -24,10 +25,12 @@ const ProductCard = ({product}) => {
               </div>
               <div className="flex items-end justify-between mt-3">
                   <p className="md:text-xl text-base font-medium text-primary">
-                      {currency}{product.offerPrice}{" "} <span className="text-gray-500/60 md:text-sm text-xs line-through">
+                      {currency}{product.offerPrice}{" "}
+                       <span className="text-gray-500/60 md:text-sm text-xs line-through">
                       {currency}{product.price}</span>
                   </p>
-                  <div onClick={(e) => {e.stopPropagation();}} className="text-primary">
+                  <div onClick={(e) => {e.stopPropagation();}}
+                   className="text-primary">
                       {!cartItems[product._id] ? (
                           <button
                         className="flex items-center justify-center gap-1 bg-primary/10 border border-primary/40 md:w-[80px] w-[64px] h-[34px] rounded cursor-pointer" 
@@ -37,11 +40,13 @@ const ProductCard = ({product}) => {
                           </button>
                       ) : (
                           <div className="flex items-center justify-center gap-2 md:w-20 w-16 h-[34px] bg-primary/25 rounded select-none">
-                              <button onClick={() => {removeFromCart(product._id)}} className="cursor-pointer text-md px-2 h-full" >
+                              <button onClick={() => {removeFromCart(product._id)}} 
+                              className="cursor-pointer text-md px-2 h-full" >
                                   -
                               </button>
                               <span className="w-5 text-center">{cartItems[product._id]}</span>
-                              <button onClick={() => {addToCart(product._id)}} className="cursor-pointer text-md px-2 h-full" >
+                              <button onClick={() => {addToCart(product._id)}}
+                               className="cursor-pointer text-md px-2 h-full" >
                                   +
                               </button>
                           </div>
